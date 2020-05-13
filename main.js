7e14e4412459a8561a98527b4aaa5209fba35500
Ewa Szczepanska-Jankowska <eszczepanska23@gmail.com>
2020-05-13 11:16:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+alert('Cześć! Miło Cię widzieć!');
+const firstName = 'Ewa'
+const age = 37
+const myPassion = 'szydełkowanie'
+const hobby = 'narciarstwo'
+console.log(`Cześć! Nazywam się ${firstName} i mam ${age} lat! Moja pasja to ${myPassion} a moje hobby to ${hobby}.`)
